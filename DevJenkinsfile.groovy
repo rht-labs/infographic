c1e8fb060ac254b61e5bda8e4088cd6497b53aa7
Justin Holmes <justinmichaelholmes@gmail.com>
2016-08-23 12:57:23 -0600

    add dev pipe

---
+++ DevJenkinsfile.groovy
@@ -0,0 +1,7 @@
+developmentPipeline{
+    buildCommands = []
+    ocHost = 'env3-1-master.innovation.labs.redhat.com'
+    projectName = 'infographic'
+    appName = 'infographic'
+    dockerRegistry = 'registry.env3-1.innovation.labs.redhat.com'
+}
