f1874f76f9a7ea7f9279ee971169aed0e615770a
Justin Holmes <justinmichaelholmes@gmail.com>
2016-08-23 23:19:52 -0600

    disabling CI for now

--- DevJenkinsfile.groovy
+++ DevJenkinsfile.groovy
@@ -1,6 +1,6 @@
 developmentPipeline{
-    buildCommands = ['npm install','npm run-script ci']
-    unitTestCommand = 'npm test'
+    buildCommands = []//['npm install','npm run-script ci'] TODO fix Jenkins tools
+    unitTestCommand = //'npm test'
     ocHost = 'env3-1-master.innovation.labs.redhat.com'
     projectName = 'infographic'
     appName = 'infographic'
