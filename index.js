ff07f3201fadbd647f5496487981782f2ef99650
Deven Phillips <deven.phillips@redhat.com>
2017-10-23 07:56:35 -0400

    License headers and file

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-
+/*-
+ * Infographic Infrastructure Automation Demo
+ * %%
+ * Copyright (C) 2016 - 2017 Red Hat Inc., Open Innovation Labs
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
 
 const httpPort = process.env.INFOGRAPHIC_HTTP_PORT || 8080;
-
 const httpsPort = process.env.INFOGRAPHIC_HTTPS_PORT || 8443;
 const httpsKey = process.env.INFOGRAPHIC_HTTPS_KEY || '';
 const httpsCert = process.env.INFOGRAPHIC_HTTPS_CERTIFICATE || '';
-
 const websiteDir = process.env.INFOGRAPHIC_DIR || 'website';
 
 var express = require('express');
